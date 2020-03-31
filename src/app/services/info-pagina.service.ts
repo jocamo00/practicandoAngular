00ad5d8622d206922b8d037aeb5f723ff876028d
Jose <jose.carreres.81@gmail.com>
2020-03-31 04:22:56 +0200

    Conexión con firebase para traernos los datos

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -11,16 +11,28 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
 
   constructor( private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    // console.log('Servicio de infoPagina listo');
-
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
           this.cargada = true;
           this.info = resp;
+        });
+  }
+
+  private cargarEquipo() {
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-9f6ac.firebaseio.com/equipo.json')
+        .subscribe( (resp: any) => {
+          this.equipo = resp;
           console.log(resp);
         });
   }
