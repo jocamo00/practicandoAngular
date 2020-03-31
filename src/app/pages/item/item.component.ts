3e65c1e9d22bb9817554496d0dca76e9fc6a4337
Jose <jose.carreres.81@gmail.com>
2020-03-31 21:03:11 +0200

    Obtiene el id del item pulsado

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
+
+    // Subscribe estar pendiente de todos los cambios que se realicen el los parametros
+    this.route.params
+        .subscribe( parametros => {
+          // Solo queremos seleccionar el id
+          // tslint:disable-next-line: no-string-literal
+          console.log(parametros['id']);
+        });
   }
 
 }
